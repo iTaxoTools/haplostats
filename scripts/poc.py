2d709328db5a248114a83d2edb406bd5105e0281
StefanPatman <stefanpatman91@gmail.com>
2023-08-16 12:00:28 +0300

    Integrate poc script as main submodule

--- scripts/poc.py
+++ scripts/poc.py
@@ -1,8 +1,6 @@
 import yaml
 
-from itaxotools.haplostats.sets import TaggedDisjointSets
-from itaxotools.haplostats.indexer import StringIndexer
-from itaxotools.haplostats.counters import TagCounters
+from itaxotools.haplostats import HaploStats
 
 
 def dict_representer(dumper, data):
@@ -18,15 +16,7 @@ def yamlify(data, title: str = None) -> str:
     return yaml.dump(data, default_flow_style=False)
 
 
-def format_haplotype_id(id: int, digits: int = 0) -> str:
-    return 'Hap' + str(id).rjust(digits, '0')
-
-
-def format_set_id(id: int, digits: int = 0) -> str:
-    return 'FOR' + str(id).rjust(digits, '0')
-
-
-def _raw_input_generator_sample():
+def raw_input_generator():
     yield ('specimen1', 'verrucosa',  'AAAAAAAAA', 'AAAAAAAAC')
     yield ('specimen2', 'verrucosa',  'AAAAAAAAC', 'AAAAAAAAG')
     yield ('specimen3', 'verrucosa',  'AAAAAAAAG', 'AAAAAAAAA')
@@ -35,101 +25,67 @@ def _raw_input_generator_sample():
     yield ('specimen6', 'enigmatica', 'GGGGGGGGG', 'CCCCCCCCT')
 
 
-def input_generator_sample():
-    for _, species, seqa, seqb in _raw_input_generator_sample():
+def extract_input_data(raw_input):
+    for _, species, seqa, seqb in raw_input:
         yield (species, [seqa, seqb])
 
 
-print()
+def format_input(raw_input):
+    return [
+        dict(
+            id = id,
+            species = species,
+            allele_a = seqa,
+            allele_b = seqb)
+        for id, species, seqa, seqb in raw_input
+    ]
 
-data = [
-    dict(id = id,
-        species = species,
-        allele_a = seqa,
-        allele_b = seqb)
-    for id, species, seqa, seqb in _raw_input_generator_sample()
-]
-print(yamlify(data, 'Input'))
-print()
 
-input = input_generator_sample()
+def main():
+    print()
 
-indexer = StringIndexer()
-counters = TagCounters()
-fors = TaggedDisjointSets()
+    data = format_input(raw_input_generator())
+    print(yamlify(data, 'Input'))
+    print()
 
-for tag, seqs in input:
-    ids = [indexer.add(seq) for seq in seqs]
-    counters.update(tag, ids)
-    fors.add(tag, ids)
+    stats = HaploStats()
+    stats.set_subset_labels(
+        subset_a = 'species_a',
+        subset_b = 'species_b',
+        subsets = 'species',
+    )
 
-haplotype_digits = len(str(len(indexer))) + 1
-set_digits = len(str(len(fors.get_set_members()))) + 1
+    for species, sequences in extract_input_data(raw_input_generator()):
+        stats.add(species, sequences)
 
-data = {
-    format_haplotype_id(id, haplotype_digits): seq
-    for id, seq in indexer.all()}
-print(yamlify(data, 'Haplotype sequences'))
-print()
+    data = stats.get_haplotypes()
+    print(yamlify(data, 'Haplotype sequences'))
+    print()
 
-data = {
-    tag: {
-        'total': counter.total(),
-        'haplotypes': {
-            format_haplotype_id(id, haplotype_digits): count
-            for id, count in counter.items()}}
-    for tag, counter in counters.all()}
-print(yamlify(data, 'Haplotypes per species'))
-print()
+    data = stats.get_haplotypes_per_subset()
+    print(yamlify(data, 'Haplotypes per species'))
+    print()
 
-data = [
-    {
-        'species_a': x,
-        'species_b': y,
-        'common': {
-            format_haplotype_id(id, haplotype_digits): count
-            for id, count in haplotypes.items()},
-    }
-    for x, y, haplotypes in counters.all_pairs()
-]
-print(yamlify(data, 'Haplotypes shared between species'))
-print()
+    data = stats.get_haplotypes_shared_between_subsets()
+    print(yamlify(data, 'Haplotypes shared between species'))
+    print()
 
-data = {
-    format_set_id(set, set_digits): [
-        format_haplotype_id(id, haplotype_digits)
-        for id in haplotypes]
-    for set, haplotypes in enumerate(fors.get_set_members())
-}
-print(yamlify(data, 'Fields of recombination'))
-print()
+    data = stats.get_fields_of_recombination()
+    print(yamlify(data, 'Fields of recombination'))
+    print()
 
-data = {
-    format_set_id(set, set_digits): {
-        'total': tags.total(),
-        'species': dict(tags)}
-    for set, tags in fors.get_tags_per_set().items()}
-print(yamlify(data, 'Species count per FOR'))
-print()
+    data = stats.get_subsets_per_field_of_recombination()
+    print(yamlify(data, 'Species count per FOR'))
+    print()
 
-data = {
-    tag: {
-        'total': sets.total(),
-        'FORs': {
-            format_set_id(set, set_digits): count
-            for set, count in sets.items()}}
-    for tag, sets in fors.get_sets_per_tag().items()}
-print(yamlify(data, 'FOR count per species'))
-print()
+    data = stats.get_fields_of_recombination_per_subset()
+    print(yamlify(data, 'FOR count per species'))
+    print()
 
-data = [
-    {
-        'species_a': x,
-        'species_b': y,
-        'common': {
-            format_set_id(set, set_digits): count
-            for set, count in sets.items()},
-    }
-    for x, y, sets in fors.get_sets_per_tag_pair()]
-print(yamlify(data, 'FORs shared between species'))
-print()
+    data = stats.get_fields_of_recombination_shared_between_subsets()
+    print(yamlify(data, 'FORs shared between species'))
+    print()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/itaxotools/haplostats/__init__.py
@@ -0,0 +1,1 @@
+from .haplostats import HaploStats
